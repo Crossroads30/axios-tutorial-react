82d8eaae1e5bbbd752b56c1f97e2efc39fd9205d
crossroads30 <crossroads30@mail.ru>
2024-01-10 20:43:37 +0300

    feat: add ex of adding headers

--- src/examples/2-headers.jsx
+++ src/examples/2-headers.jsx
@@ -8,7 +8,18 @@ const Headers = () => {
   const [joke, setJoke] = useState('random dad joke');
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    try {
+      // const response = await axios(url, {
+      const { data } = await axios(url, { // destructuring for 'response.data'
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+      console.log(data.joke)
+      setJoke(data.joke)
+    } catch (error) {
+      console.log(error.response)
+    }
   };
 
   return (
